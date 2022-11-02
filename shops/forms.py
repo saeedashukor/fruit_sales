a6b2822e6dac4c33ce2bca1ffb06e628f851a24f
Saeeda <saeedashukor360@gmail.com>
2022-11-02 16:17:04 +0000

    added modelforms and deleteview

--- shops/forms.py
+++ shops/forms.py
@@ -1,22 +1,19 @@
+from django.forms import ModelForm
 from django import forms
 import datetime
-from .models import City
+from .models import City, Shop
 
 
-class CityForm(forms.Form):
-    name = forms.CharField(label='City name', max_length=100)
+class CityForm(ModelForm):
+    class Meta:
+        model = City
+        fields = ['name']
+        labels = {'name': ('City name')}
 
 
-class ShopForm(forms.Form):
-    class CityNameChoiceField(forms.ModelChoiceField):
-        def label_from_instance(self, obj):
-            return f'{obj.name}'
-
-    city = CityNameChoiceField(queryset=City.objects.all())
-    name = forms.CharField(label='Shop name', max_length=200)
-    code = forms.CharField(label='Shop code, eg: ShopA, Shop9', max_length=10)
-    address = forms.CharField(label='Address', max_length=300)
-    postcode = forms.CharField(label='Postcode', max_length=5)
+class ShopForm(ModelForm):
     year_opened = forms.IntegerField(label='Year opened', min_value=1900, max_value=datetime.datetime.now().year)
 
-
+    class Meta:
+        model = Shop
+        fields = ['city', 'name', 'code', 'address', 'postcode', 'year_opened']
--- shops/models.py
+++ shops/models.py
@@ -4,11 +4,17 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Shop(models.Model):
     city = models.ForeignKey(City, on_delete=models.PROTECT)
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=10)
     address = models.TextField(max_length=300, null=True, blank=True)
-    postcode = models.CharField(max_length=10, null=True, blank=True)
-    year_opened = models.IntegerField(null=True, blank=True)
+    postcode = models.CharField(max_length=5, null=True, blank=True)
+    year_opened = models.IntegerField(max_length=4, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}, {self.code}, {self.city}, {self.address}, {self.postcode}, {self.year_opened}'
